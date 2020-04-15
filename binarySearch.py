af7029051125f4db97eb792f15113c61fbaba9e3
Vladislav <guru-drakuru@yandex.ru>
2020-04-15 22:12:25 +0300

    trying binary search

---
+++ binarySearch.py
@@ -0,0 +1,20 @@
+def binarySearch (list, item):
+    low = 0
+    hight = len(list) - 1
+    i = 0
+    while low <= hight:
+        mid = (low + hight)//2
+        guess = list[mid]
+        if guess == item:
+            return i
+        if guess > item:
+            hight = mid - 1
+            i += 1
+        else:
+            low = mid + 1
+            i += 1
+    return None
+
+
+numbers = [i for i in range(0, 257)]
+print(binarySearch(numbers, 256))
